c5e4c58d6bb97a2f87993384ce8171ae47d1f0d5
root <root@Laptop-Ehsan>
2024-09-28 15:30:58 +0330

    Add cart button

--- src/Constants/colors.js
+++ src/Constants/colors.js
@@ -1,3 +1,5 @@
 export const COLORS = {
-  Nav_Item_Hover: "#e5e5e5",
+  Nav_Border: "hsl(0 0% 65%)",
+  Nav_Item_Hover: "hsl(0 0% 96%)",
+  Cart_Icon_Hover: "hsl(70 60% 99%)",
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,18 @@
 import styled from "styled-components";
 import { COLORS } from "../Constants/colors";
+import { CART_ICON } from "../assets/icons/svg";
 
 function Header() {
   return (
     <Container>
       <Nav>
-        <Logo></Logo>
+        <Logo>Neo</Logo>
         <Group>
           <Item>Home</Item>
           <Item>Products</Item>
-          <Item></Item>
+          <Item>
+            <CartLink>{CART_ICON}</CartLink>
+          </Item>
         </Group>
       </Nav>
     </Container>
@@ -20,17 +23,27 @@ export default Header;
 
 const Container = styled.header`
   position: sticky;
+  max-width: 800px;
+  margin: 0 auto;
 `;
 
 const Nav = styled.nav`
   display: flex;
   justify-content: space-between;
+  align-items: center;
+  padding: 10px 15px;
+  border: 1px solid ${COLORS.Nav_Border};
+  border-radius: 30px;
+  user-select: none;
 `;
 
-const Logo = styled.h1``;
+const Logo = styled.h1`
+  cursor: pointer;
+`;
 
 const Group = styled.ul`
   display: flex;
+  gap: 10px;
   list-style: none;
 `;
 
@@ -40,9 +53,16 @@ const Item = styled.li`
   border-radius: 5px;
 
   cursor: pointer;
-  user-select: none;
 
   &:hover {
     background-color: ${COLORS.Nav_Item_Hover};
   }
+`;
+
+const CartLink = styled.a`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
 `;

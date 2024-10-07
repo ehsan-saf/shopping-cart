413a0a58d50c610ac0f0765917a4d8a00c99cccb
root <root@Laptop-Ehsan>
2024-10-07 17:53:48 +0330

    Add buy button to product page

--- src/components/subComponents/ProductPage.jsx
+++ src/components/subComponents/ProductPage.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { useLocation, useParams } from "react-router-dom";
+import { COLORS } from "../../Constants/colors";
 
 function ProductPage() {
   const { id } = useParams();
@@ -11,6 +12,8 @@ function ProductPage() {
     <Container>
       <Info>
         <h3>{data.title}</h3>
+        <p>$ {data.price}</p>
+        <AddButton>Add to cart</AddButton>
       </Info>
       <Image src={data.image} alt={data.title} />
     </Container>
@@ -24,10 +27,16 @@ const Container = styled.div`
   margin: 0 auto;
   display: flex;
   justify-content: space-between;
+
+  @media (max-width: 600px) {
+    flex-direction: column-reverse;
+    align-items: center;
+  }
 `;
 
 const Info = styled.div`
   display: flex;
+  flex-direction: column;
 
   & h3 {
     max-width: 300px;
@@ -38,4 +47,19 @@ const Info = styled.div`
 const Image = styled.img`
   max-width: 250px;
   height: auto;
+`;
+
+const AddButton = styled.button`
+  padding: 10px;
+  border: none;
+  border-radius: 10px;
+
+  color: white;
+  background: ${COLORS.Default_Button};
+
+  font-size: 1.3rem;
+
+  &:hover {
+    background: ${COLORS.Default_Button_Hover};
+  }
 `;
